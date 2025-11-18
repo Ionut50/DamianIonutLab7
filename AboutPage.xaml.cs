696805389145e4badd6503fd4e1f7a5af560545e
damian ionut <ionutdamian50@hotmail.com>
2025-11-18 11:08:12 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace DamianIonutLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DamianIonutLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
