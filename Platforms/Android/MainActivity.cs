a2dd9af22213742c7a09609defe835e17a1bf196
damian ionut <ionutdamian50@hotmail.com>
2025-12-05 11:54:14 +0200

    lab20update

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -2,10 +2,29 @@
 using Android.Content.PM;
 using Android.OS;
 
+using Plugin.LocalNotification;
+using Android.Content;
+
 namespace DamianIonutLab7
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // Apelul către bază trebuie să fie primul
+            base.OnCreate(savedInstanceState);
+
+            // Adaugă această linie pentru a inițializa pachetul de notificări
+            LocalNotificationCenter.CreateNotificationChannel();
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+
+            // Adaugă această linie pentru a gestiona notificările la deschiderea aplicației
+            LocalNotificationCenter.NotifyNotificationTapped(intent);
+        }
     }
 }
